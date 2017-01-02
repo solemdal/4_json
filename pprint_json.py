94a1ec8aed26f859e39b6eb3d78918f76850ee46
solemdal <temarual@gmail.com>
2017-01-02 12:45:27 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,35 @@
 import json
+import sys
+import os.path
 
 
 def load_data(filepath):
-    pass
+    if os.path.exists(filepath):
+        with open(filepath, "r", encoding="utf-8") as input_file:
+            filedata = json.load(input_file)
+            return filedata
+    else:
+        return None
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(data_):
+    print(json.dumps(data_, indent=4, ensure_ascii=False, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 2:
+        path_to_file = sys.argv[1]
+    else:
+        print("You should specify a file to analyze!")
+        sys.exit(0)
+    if load_data(path_to_file):
+        data = load_data(path_to_file)
+    else:
+        print("Incorrect path to the file!")
+        sys.exit(0)
+
+    # with open("output.txt", "w") as output_file:
+    #     output_file.write(pretty_print_json(data))
+    # print('Привет')
+
+    pretty_print_json(data)
